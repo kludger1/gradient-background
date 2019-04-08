af194c6dea8e9506750cc32b6d81dcd89d90ee96
unknown <ludgerk@techlaunch.com>
2019-04-08 00:02:50 -0400

    i fixed html, changed some class name, change the style, finished the javascript, did all the basic features now i just need to make js code look way better and dry it up

--- script.js
+++ script.js
@@ -1,19 +1,116 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const body = document.querySelector("body");
+const numInput = document.querySelector(".num-input")
+const option = document.querySelector(".options");
+const enterBtn = document.querySelector(".enterBtn")
+let linearAngle = option.options[option.selectedIndex].value;
+const p = document.querySelector(".p-tag");
+const css = document.querySelector("h3");
+const div = document.querySelector(".color-inputs-container")
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
 
-	css.textContent = body.style.background + ";";
+console.log(p)
+const input = 2;
+
+
+
+function getrandomHex() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient);
+function makeInput(num) {
+	const inputs = [];
+
+	for (let i = 1; i <= num; i++) {
+		const input = document.createElement("input");
+		input.type = "color";
+		input.name = `color${[i]}`;
+		input.value = getrandomHex();;
+		div.appendChild(input);
+
+
+		const inputArr = [];
+		inputArr.push(input.name, input.value)
+		inputs.push(inputArr)
+	}
+	return inputs
+}
+
+let inputs = makeInput(input)
+
+
+
+
+
+function setGradient(angle, arr) {
+	p.classList.add("view");
+	bg = [`linear-gradient(${angle}`]
+
+	for (let i = 0; i < arr.length; i++) {
+		// bg[i + 1] = " " + arr[i][1];
+		bg.push(arr[i][1]);
+		console.log(arr[i][1])
+	}
+	bg.push(")")
+	let bgStr = bg.join(",")
+	let bgStr2 = bgStr.substring(0, bgStr.length - 2) + ')'
+	// console.log(bgStr.substring(0, bgStr.length - 2));
+	console.log(bgStr2)
+	body.style.background = bgStr2
+	css.textContent = bgStr2;
+}
+
+setGradient(linearAngle, inputs)
+
+
+
+
+document.addEventListener("input", event => {
+	var element = event.target;
+	// console.log(element.name)
+	// console.log(element.value)
+	for (var i in inputs) {
+		if (inputs[i][0] === element.name) {
+			inputs[i][1] = element.value
+			setGradient(linearAngle, inputs)
+
+		}
+	}
+});
+
+numInput.addEventListener("keydown", (e) => {
+	if (e.keyCode === 13 && numInput.value >= 2) {
+		div.innerHTML = ""
+		inputs = makeInput(numInput.value)
+		setGradient(option.options[option.selectedIndex].value, inputs)
+		console.log(option.options[option.selectedIndex].value)
+
+	}
+
+})
+
+enterBtn.addEventListener("click", (e) => {
+	// e.preventDefault()
+	if (numInput.value >= 2) {
+		div.innerHTML = ""
+		inputs = makeInput(numInput.value)
+		console.log(option.options[option.selectedIndex].value)
+		setGradient(option.options[option.selectedIndex].value, inputs)
+	}
+
+})
+
+
+
+css.onclick = function () {
+	document.execCommand("copy");
+}
+
+css.addEventListener("copy", function (event) {
+	event.preventDefault();
+	p.classList.remove("view");
+	if (event.clipboardData) {
+		event.clipboardData.setData("text/plain", css.textContent);
+	}
+	
+});
