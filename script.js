bf436e1e412be5c17607f5e2bf796f9e8a54ac01
unknown <ludgerk@techlaunch.com>
2019-04-11 00:46:01 -0400

    fixed code up less messy, fixed option bug, switch from arr to obj

--- script.js
+++ script.js
@@ -1,116 +1,96 @@
 const body = document.querySelector("body");
-const numInput = document.querySelector(".num-input")
+const inputsContainer = document.querySelector(".inputs");
+const numInput = document.querySelector(".num-input");
 const option = document.querySelector(".options");
-const enterBtn = document.querySelector(".enterBtn")
-let linearAngle = option.options[option.selectedIndex].value;
+// const linearAngle = option.options[option.selectedIndex];
+const randomizeBtn = document.querySelector(".randomize-btn")
+const colorInputsContainer = document.querySelector(".color-inputs-container");
 const p = document.querySelector(".p-tag");
-const css = document.querySelector("h3");
-const div = document.querySelector(".color-inputs-container")
-
-
-console.log(p)
-const input = 2;
+const h3 = document.querySelector("h3");
 
 
 
-function getrandomHex() {
+
+
+const getrandomHex = () => {
 	return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 
-function makeInput(num) {
-	const inputs = [];
+const makeColorInput = (num) => {
+	const colorInputValues = {}
 
 	for (let i = 1; i <= num; i++) {
-		const input = document.createElement("input");
-		input.type = "color";
-		input.name = `color${[i]}`;
-		input.value = getrandomHex();;
-		div.appendChild(input);
+		const colorInput = document.createElement("input");
+		colorInput.type = "color";
+		colorInput.name = `color${[i]}`;
+		colorInput.value = getrandomHex();;
+		colorInputsContainer.appendChild(colorInput);
 
-
-		const inputArr = [];
-		inputArr.push(input.name, input.value)
-		inputs.push(inputArr)
+		colorInputValues[colorInput.name] = colorInput.value
 	}
-	return inputs
+	return colorInputValues
 }
 
-let inputs = makeInput(input)
 
-
-
-
-
-function setGradient(angle, arr) {
+const setGradient = (angle, obj) => {
 	p.classList.add("view");
-	bg = [`linear-gradient(${angle}`]
+	linearGradient = [`linear-gradient(${angle}`]
 
-	for (let i = 0; i < arr.length; i++) {
-		// bg[i + 1] = " " + arr[i][1];
-		bg.push(arr[i][1]);
-		console.log(arr[i][1])
+	for (let props in obj) {
+		linearGradient.push(obj[props])
 	}
-	bg.push(")")
-	let bgStr = bg.join(",")
-	let bgStr2 = bgStr.substring(0, bgStr.length - 2) + ')'
-	// console.log(bgStr.substring(0, bgStr.length - 2));
-	console.log(bgStr2)
-	body.style.background = bgStr2
-	css.textContent = bgStr2;
+
+	linearGradientStr = linearGradient.join().concat(")")
+	body.style.background = linearGradientStr
+	h3.textContent = linearGradientStr;
 }
 
-setGradient(linearAngle, inputs)
 
 
-
-
-document.addEventListener("input", event => {
-	var element = event.target;
-	// console.log(element.name)
-	// console.log(element.value)
-	for (var i in inputs) {
-		if (inputs[i][0] === element.name) {
-			inputs[i][1] = element.value
-			setGradient(linearAngle, inputs)
-
+const changeGradientColor = event => {
+	const element = event.target;
+	for (let props in obj) {
+		if (props === element.name) {
+			obj[props] = element.value
+			setGradient(option.options[option.selectedIndex].value, obj)
 		}
-	}
-});
-
-numInput.addEventListener("keydown", (e) => {
-	if (e.keyCode === 13 && numInput.value >= 2) {
-		div.innerHTML = ""
-		inputs = makeInput(numInput.value)
-		setGradient(option.options[option.selectedIndex].value, inputs)
-		console.log(option.options[option.selectedIndex].value)
 
 	}
+}
 
-})
 
-enterBtn.addEventListener("click", (e) => {
-	// e.preventDefault()
-	if (numInput.value >= 2) {
-		div.innerHTML = ""
-		inputs = makeInput(numInput.value)
-		console.log(option.options[option.selectedIndex].value)
-		setGradient(option.options[option.selectedIndex].value, inputs)
+const randomize = () => {
+	if (numInput.value >= 2 && numInput.value <= 100) {
+		console.log("ran")
+		colorInputsContainer.innerHTML = "";
+		obj = makeColorInput(numInput.value);
+		setGradient(option.options[option.selectedIndex].value, obj);
 	}
-
-})
-
+}
 
 
-css.onclick = function () {
+h3.onclick = function () {
 	document.execCommand("copy");
 }
 
-css.addEventListener("copy", function (event) {
+
+const copy = (event) => {
 	event.preventDefault();
 	p.classList.remove("view");
 	if (event.clipboardData) {
-		event.clipboardData.setData("text/plain", css.textContent);
+		event.clipboardData.setData("text/plain", h3.textContent);
 	}
-	
-});
+}
+
+
+
+inputsContainer.addEventListener("change", () => changeGradientColor(event));
+randomizeBtn.addEventListener("click", randomize);
+option.addEventListener("change", () => setGradient(option.options[option.selectedIndex].value, obj))
+h3.addEventListener("copy", copy);
+
+
+let obj = makeColorInput(numInput.value);
+setGradient(option.options[option.selectedIndex].value, obj);
+
